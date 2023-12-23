a5b3ea024b648d1c9023020f697eeb2bf6fe5600
cristi <mastercris78@yahoo.com>
2023-12-23 16:54:04 +0200

    Small fixes

--- lambda/decode_function.py
+++ lambda/decode_function.py
@@ -8,7 +8,7 @@ class IBucketReader(ABC):
         pass
 
     @abstractmethod
-    def read_object_content(self, params=dict):
+    def read_object_content(self, bucket=object, object_key=str):
         pass
 
 
@@ -16,8 +16,8 @@ class BucketReader(IBucketReader):
     def __init__(self):
         self.s3 = boto3.client("s3")
 
-    def read_object_content(self, params=dict):
-        bucket_object = self.s3.get_object(Bucket=params[0], Key=params[1])
+    def read_object_content(self, bucket=object, object_key=str):
+        bucket_object = self.s3.get_object(Bucket=bucket, Key=object_key)
         object_data = bucket_object['Body'].read()
         object_str = object_data.decode('utf-8')
         return object_str
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,12 +1,10 @@
 from decode_function import BucketReader
-from event_reader import EventReader
 from mail_client import MailService
 
 
 def lambda_handler(event, context):
-    event_reader = EventReader()
     bucket_reader = BucketReader()
     mail_sender = MailService()
-    address_str = bucket_reader.read_object_content(event_reader.get_object(event=event, key="address"))
-    content_str = bucket_reader.read_object_content(event_reader.get_object(event=event, key="content"))
+    address_str = bucket_reader.read_object_content(bucket=event["bucket"], object_key=["address"])
+    content_str = bucket_reader.read_object_content(bucket=event["bucket"], object_key=["content"])
     mail_sender.send_mail(source_address=address_str, destination_address=address_str, content=content_str)
