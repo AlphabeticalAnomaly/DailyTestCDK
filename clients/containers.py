e26e84583215b15690f238229e0d2ec74eb28153
cristi <mastercris78@yahoo.com>
2024-01-06 11:45:33 +0200

    mock tests

---
+++ clients/containers.py
@@ -0,0 +1,26 @@
+from dependency_injector import containers, providers
+import boto3.session
+
+
+class Container(containers.DeclarativeContainer):
+
+    # wiring_config = containers.WiringConfiguration(modules=["decode_function.py", "mail_client.py"])
+
+    session = providers.Resource(
+        boto3.session.Session
+    )
+
+    s3_client = providers.Singleton(
+        session.provided.call(),
+        service_name='s3',
+
+    )
+
+    ses_client = providers.Singleton(
+        session.provided.call(),
+        service_name='ses',
+    )
+
+
+
+
