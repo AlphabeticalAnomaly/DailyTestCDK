f3feb3fcc1a1a86b3658b7e1b9cda158638538a8
Cristi <mastercris78@yahoo.com>
2024-02-20 15:40:45 +0200

    modified test_handler and flask app

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,24 +1,16 @@
-from decode_function import BucketReader
-from mail_client import MailService
-from dynamo_client import DynamoClient
-from datetime import datetime
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 import awsgi
 import json
+from app_task import send_email
+
 
 app = Flask("DailymailStack")
 
 
 @app.route('/', methods=['POST'])
-def send_email():
+def task():
     data = json.loads(request.get_data().decode('utf-8'))
-    bucket = data["bucket"]
-    address = data["address"]
-    content = data["content"]
-    current_date = datetime.today().strftime('%Y.%m.%d.%H.%M.%S')
-    content_str = BucketReader().read_object_content(bucket=bucket, object_key=content)
-    DynamoClient(table="DailyTable").dynamo_put_item(item={"email": address, "date": current_date, "content": content})
-    MailService().send_email(source_address=address, destination_address=address, content=content_str)
+    send_email(data=data)
     return jsonify(status=200, message="Email sent")
 
 
--- src/test_handler.py
+++ src/test_handler.py
@@ -1,18 +1,19 @@
-import unittest
 from unittest.mock import Mock
-from lambda_function import lambda_handler
+import json
+import unittest
+from app_task import send_email
 from datetime import datetime
 
 
 class TestHandler(unittest.TestCase):
-    def test_handler(self):
-        event = {"bucket": "test_bucket", "content": "test_content", "address": "test_address"}
-        context = Mock()
+
+    def test_send_email(self):
+        current_date = datetime.today().strftime('%Y.%m.%d.%H.%M.%S')
+        dict = {"bucket": "test_bucket", "content": "test_content", "address": "test_address"}
         reader = Mock()
         mailer = Mock()
         dynamo = Mock()
-        current_date = datetime.today().strftime('%Y.%m.%d.%H.%M.%S')
-        handler = lambda_handler(event=event, context=context, reader=reader, mailer=mailer, dynamo=dynamo)
-        reader.read_object_content.assert_called_with(bucket=event["bucket"], object_key=event["content"])
-        mailer.send_email.assert_called_with(source_address=event["address"], destination_address=event["address"], content=reader.read_object_content.return_value)
-        dynamo.dynamo_put_item.assert_called_with(item={"email": event["address"], "date": current_date, "content": event["content"]})
+        send_email(data=dict, reader=reader, mailer=mailer, dynamo=dynamo)
+        reader.read_object_content.assert_called_with(bucket="test_bucket", object_key="test_content")
+        mailer.send_email.assert_called_with(source_address="test_address", destination_address="test_address", content=reader.read_object_content.return_value)
+        dynamo.dynamo_put_item.assert_called_with(item={"email": "test_address", "date": current_date, "content": "test_content"})
